7051493f28d82cdd552943dd3853b5382752f916
gerardobarciap <gerardobarciap@beabdc1e-be5c-11dd-b9c8-e7e19bf6fede>
2008-11-30 03:43:14 +0000

    Primera version lista del caso de uso Agregar Nuevo Horario

--- src/com/wd/gui/controlparticular/ControlGuiHorario.java
+++ src/com/wd/gui/controlparticular/ControlGuiHorario.java
@@ -2,7 +2,6 @@
 package com.wd.gui.controlparticular;
 
 import com.wd.dominio.Horario;
-
 import com.wd.gui.ControlGui;
 import com.wd.servicios.ControlGeneral;
 
@@ -12,15 +11,24 @@ import com.wd.servicios.ControlGeneral;
  */
 public class ControlGuiHorario {
 
+/** variable para el manejo de la entidad horario */
     private Horario horario;
+
+/** variable para el manejo de la instancia del controlador GUI */
     private ControlGui controlador;
 
+/** constructor que inicia el control general GUI */
     public ControlGuiHorario() {
         this.controlador = new ControlGui();
     }
 
-
-
+/**
+ * Metodo para agregar un horario al sistema
+ * @param horaIni int hora de inicio
+ * @param horaFin int hora de fin
+ * @param diaIni String dia de inicio
+ * @param diaFin String dia de Fin
+ */
     public void  agregarHorarioalSistema (int horaIni, int horaFin, String diaIni,
                             String diaFin) {
         horario = new Horario(0,horaIni,horaFin,diaIni,diaFin);
