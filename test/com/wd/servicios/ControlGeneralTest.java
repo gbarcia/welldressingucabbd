721aed8724d0dc325a64a6802ae842764856fe01
trujillo.jonathan <trujillo.jonathan@beabdc1e-be5c-11dd-b9c8-e7e19bf6fede>
2008-12-03 20:23:28 +0000

    Caso de Uso: Agregar Tienda (Nota: Interfaz aun implementada)

--- test/com/wd/servicios/ControlGeneralTest.java
+++ test/com/wd/servicios/ControlGeneralTest.java
@@ -5,6 +5,9 @@
 
 package com.wd.servicios;
 
+import com.wd.dominio.Horario;
+import com.wd.dominio.Tienda;
+import java.util.Collection;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -47,5 +50,62 @@ public class ControlGeneralTest {
         assertNotNull(result);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getInstance method, of class ControlGeneral.
+     */
+    @Test
+    public void testGetInstance1() {
+        System.out.println("getInstance");
+        ControlGeneral expResult = null;
+        ControlGeneral result = ControlGeneral.getInstance();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of agregarNuevoHorario method, of class ControlGeneral.
+     */
+    @Test
+    public void testAgregarNuevoHorario() {
+        System.out.println("agregarNuevoHorario");
+        Horario horario = null;
+        ControlGeneral instance = null;
+        boolean expResult = false;
+        boolean result = instance.agregarNuevoHorario(horario);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of traerTodosLosHorarios method, of class ControlGeneral.
+     */
+    @Test
+    public void testTraerTodosLosHorarios() {
+        System.out.println("traerTodosLosHorarios");
+        ControlGeneral instance = null;
+        Collection expResult = null;
+        Collection result = instance.traerTodosLosHorarios();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of agregarNuevaTienda method, of class ControlGeneral.
+     */
+    @Test
+    public void testAgregarNuevaTienda() {
+        System.out.println("agregarNuevaTienda");
+        Tienda tienda = null;
+        ControlGeneral instance = null;
+        boolean expResult = false;
+        boolean result = instance.agregarNuevaTienda(tienda);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
 }
