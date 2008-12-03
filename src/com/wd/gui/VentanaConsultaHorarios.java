0096aeb364e5ece563b85cb48062314de236e855
gerardobarciap <gerardobarciap@beabdc1e-be5c-11dd-b9c8-e7e19bf6fede>
2008-12-03 19:57:23 +0000

    Caso de Uso traer todos los horarios

--- src/com/wd/gui/VentanaConsultaHorarios.java
+++ src/com/wd/gui/VentanaConsultaHorarios.java
@@ -39,17 +39,6 @@ public class VentanaConsultaHorarios extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Consulta de Horarios");
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ));
         jScrollPane1.setViewportView(jTable1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
