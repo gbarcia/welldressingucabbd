ea3574e12f148a4313c27f1eecce12b6c439e410
gerardobarciap <gerardobarciap@beabdc1e-be5c-11dd-b9c8-e7e19bf6fede>
2008-12-23 17:35:32 +0000

    Dominio para Solicitudes

---
+++ src/com/wd/servicios/IfaceControlSolicitud.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.wd.servicios;
+
+/**
+ *
+ * @author Gerardo Barcia
+ */
+public class IfaceControlSolicitud {
+
+}
