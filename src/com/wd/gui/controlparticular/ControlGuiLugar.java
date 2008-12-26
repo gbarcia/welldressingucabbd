325365f9e303fe6b064a122641a62db12386931d
gerardobarciap <gerardobarciap@beabdc1e-be5c-11dd-b9c8-e7e19bf6fede>
2008-12-26 05:32:22 +0000

    actualizacion del caso de uso Lugar

--- src/com/wd/gui/controlparticular/ControlGuiLugar.java
+++ src/com/wd/gui/controlparticular/ControlGuiLugar.java
@@ -1,4 +1,3 @@
-
 package com.wd.gui.controlparticular;
 
 import com.wd.dominio.Lugar;
@@ -16,13 +15,11 @@ public class ControlGuiLugar {
 
     /** variable para el manejo de la entidad lugar */
     private Lugar lugar;
-
     /** variable para el manejo de la instancia del controlador GUI */
     private ControlGui controlador;
-
     /** variable de la interface responsable del control de las entidades */
     private IfaceControlGeneral controlG = ControlGeneral.getInstance();
-    
+
     /** constructor que inicia el control general GUI */
     public ControlGuiLugar() {
         this.controlador = new ControlGui();
@@ -33,7 +30,7 @@ public class ControlGuiLugar {
      * @param operacion int si es 1 trae todos los estads, si es 2 todas las ciudades
      * @return Vector con todas los lugares resultantes de la operacion
      */
-    public Vector<Lugar> traerTodosLosLugares (int operacion) {
+    public Vector<Lugar> traerTodosLosLugares(int operacion) {
         Vector<Lugar> resultado = null;
         Collection<Lugar> coleccion = controlG.traerTodosLosLuagres(operacion);
         resultado = new Vector();
@@ -43,7 +40,31 @@ public class ControlGuiLugar {
         return resultado;
     }
 
-    public void agregarLugarAlSistema () {
-        
+    private boolean agregarEstado(Lugar estado) {
+        boolean resultado = false;
+
+        return false;
+    }
+
+    private boolean agregarCiudad(Lugar ciudad) {
+        return false;
+    }
+
+    public void agregarLugarAlSistema(String nombrePropio, int tipo,
+            int LugarId) {
+        Lugar estado = null;
+        Lugar ciudad = null;
+        boolean resultado = false;
+        if (tipo == 0) {
+            estado = new Lugar(0, nombrePropio, tipo);
+            resultado = this.agregarEstado(estado);
+        } else {
+            ciudad = new Lugar(tipo, nombrePropio, tipo, LugarId, nombrePropio);
+            resultado = this.agregarCiudad(ciudad);
+        }
+        if (resultado) {
+        controlador.mostrarMensaje("Lugar " + nombrePropio +  "agregado con éxito",0);
+        }
+        else controlador.mostrarMensaje("Operacion fallida", 1);
     }
 }
