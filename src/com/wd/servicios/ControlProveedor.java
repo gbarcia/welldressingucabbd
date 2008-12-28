b57bf8e1a0dddc5e89705349de768deef67bf910
gerardobarciap <gerardobarciap@beabdc1e-be5c-11dd-b9c8-e7e19bf6fede>
2008-12-28 21:28:43 +0000

    actualizacion del caso de uso Proveedor

--- src/com/wd/servicios/ControlProveedor.java
+++ src/com/wd/servicios/ControlProveedor.java
@@ -1,4 +1,3 @@
-
 package com.wd.servicios;
 
 import com.ibatis.common.resources.Resources;
@@ -8,6 +7,7 @@ import com.wd.dominio.Proveedor;
 import java.io.IOException;
 import java.io.Reader;
 import java.sql.SQLException;
+import java.util.Collection;
 import org.apache.log4j.Logger;
 
 /**
@@ -18,7 +18,6 @@ public class ControlProveedor {
 
     /** Variable para trabajar con el SQLMap*/
     private SqlMapClient sqlMap;
-
     /** Variable para trabajar con la bitacora*/
     private Logger bitacora = Logger.getLogger(getClass());
 
@@ -30,12 +29,12 @@ public class ControlProveedor {
         sqlMap = SqlMapClientBuilder.buildSqlMapClient(reader);
     }
 
-     /**
-    * Operacion para agregar un proveedor al sistema
-    * @param proveedor el proveedor a registrar
-    * @return boolean resultado de la operacion
-    */
-    public boolean agregarProveedor (Proveedor proveedor) {
+    /**
+     * Operacion para agregar un proveedor al sistema
+     * @param proveedor el proveedor a registrar
+     * @return boolean resultado de la operacion
+     */
+    public boolean agregarProveedor(Proveedor proveedor) {
         boolean resultado = false;
         Integer pk = -1;
         try {
@@ -43,13 +42,69 @@ public class ControlProveedor {
             bitacora.info("Provedor: " + pk + " agregado con éxito");
             resultado = true;
         } catch (SQLException ex) {
-            bitacora.error("No se pudo agregar el proveedor " + proveedor.getNombre()
-                    + " porque " + ex.getMessage());
+            bitacora.error("No se pudo agregar el proveedor " + proveedor.getNombre() + " porque " + ex.getMessage());
             resultado = false;
+        } finally {
+            return resultado;
+        }
+    }
+
+    /**
+     * Operacion para editar un proveedor en el sistema
+     * @param proveedor el proveedor a registrar
+     * @return boolean resultado de la operacion
+     */
+    public boolean editarProveedor(Proveedor proveedor) {
+        boolean resultado = false;
+        try {
+            int rowa = sqlMap.update("actualizarProveedor", proveedor);
+            if (rowa > 0) {
+                resultado = true;
+                bitacora.info("Provedor: " + proveedor.getRif() + " actualizado con éxito");
+            }
+        } catch (SQLException ex) {
+            resultado = false;
+            bitacora.error("No se pudo actualizar el proveedor " + proveedor.getNombre()
+                    + " porque " + ex.getMessage());
+        } finally {
+            return resultado;
+        }
+    }
+
+    /**
+     * Operacion para consultar un proveedor en el sistema
+     * @param rif String rif del proveedor a consultar
+     * @return resultado Proveedor con los datos de la consulta
+     */
+    public Proveedor consultarProveedor (String rif) {
+        Proveedor resultado = null;
+        try {
+             bitacora.info("Iniciando operacion para consultar proveedor: " + rif);
+             resultado = (Proveedor) sqlMap.queryForObject("buscarUnProveedor", rif);
+        } catch (SQLException ex) {
+            bitacora.error("No se pudo realizar la operacion porque: "
+                    + ex.getMessage());
         }
         finally {
             return resultado;
         }
     }
 
+    /**
+     * Operacion para consultar todos los proveedores registrados en el sistema     
+     * @return resultado Coleccion de objetos Proveedor
+     */
+    public Collection<Proveedor> todosLosProveedores() {
+        Collection<Proveedor> resultado = null;
+        try {
+            bitacora.info("Iniciando operacion para traer todos los proveedores");
+            resultado = sqlMap.queryForList("TodosLosProveedores");
+        } catch (SQLException ex) {
+             bitacora.error("No se pudo realizar la operacion porque: "
+                    + ex.getMessage());
+        }
+        finally {
+            return resultado;
+        }
+    }
 }
--- test/com/wd/servicios/ControlProveedorTest.java
+++ test/com/wd/servicios/ControlProveedorTest.java
@@ -7,6 +7,7 @@ package com.wd.servicios;
 
 import com.wd.dominio.Proveedor;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.junit.After;
@@ -44,21 +45,98 @@ public class ControlProveedorTest {
     /**
      * Test of agregarProveedor method, of class ControlProveedor.
      */
+//    @Test
+//    public void testAgregarProveedor() {
+//        try {
+//            System.out.println("agregarProveedor");
+//            Proveedor proveedor = null;
+//            Integer n = new Integer (2);
+//            proveedor = new Proveedor("J-546", "PROVEMED", "0212", "DIR", n, "GERARDO", "barcia");
+//            ControlProveedor instance = new ControlProveedor();
+//            boolean expResult = true;
+//            boolean result = instance.agregarProveedor(proveedor);
+//            assertEquals(expResult, result);
+//            // TODO review the generated test code and remove the default call to fail.
+//            //fail("The test case is a prototype.");
+//        } catch (IOException ex) {
+//            Logger.getLogger(ControlProveedorTest.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        // TODO review the generated test code and remove the default call to fail.
+//        //fail("The test case is a prototype.");
+//    }
+
+    /**
+     * Test of agregarProveedor method, of class ControlProveedor.
+     */
+//    @Test
+//    public void testAgregarProveedor_Proveedor() {
+//        System.out.println("agregarProveedor");
+//        Proveedor proveedor = null;
+//        ControlProveedor instance = new ControlProveedor();
+//        boolean expResult = false;
+//        boolean result = instance.agregarProveedor(proveedor);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+
+    /**
+     * Test of editarProveedor method, of class ControlProveedor.
+     */
     @Test
-    public void testAgregarProveedor() {
+    public void testEditarProveedor() {
+        System.out.println("editarProveedor");
+        Proveedor proveedor = new Proveedor("J-1","POR", "TELF", "DIR", 2, "GER", "BAR");
+        ControlProveedor instance = null;
         try {
-            System.out.println("agregarProveedor");
-            Proveedor proveedor = null;
-            Integer n = new Integer (2);
-            proveedor = new Proveedor("J-546", "PROVEMED", "0212", "DIR", n, "GERARDO", "barcia");
-            ControlProveedor instance = new ControlProveedor();
-            boolean expResult = true;
-            boolean result = instance.agregarProveedor(proveedor);
-            assertEquals(expResult, result);
-            // TODO review the generated test code and remove the default call to fail.
-            //fail("The test case is a prototype.");
+            instance = new ControlProveedor();
         } catch (IOException ex) {
             Logger.getLogger(ControlProveedorTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        boolean expResult = true;
+        boolean result = instance.editarProveedor(proveedor);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of consultarProveedor method, of class ControlProveedor.
+     */
+    @Test
+    public void testConsultarProveedor() {
+        System.out.println("consultarProveedor");
+        String rif = "J-1";
+        ControlProveedor instance = null;
+        try {
+            instance = new ControlProveedor();
+        } catch (IOException ex) {
+            Logger.getLogger(ControlProveedorTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Proveedor result = instance.consultarProveedor(rif);
+        assertNotNull(result);
+        System.out.println(result.getRif());
+        System.out.println(result.getNombre());
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of todosLosProveedores method, of class ControlProveedor.
+     */
+    @Test
+    public void testTodosLosProveedores() {
+        System.out.println("todosLosProveedores");
+        ControlProveedor instance = null;
+        try {
+            instance = new ControlProveedor();
+        } catch (IOException ex) {
+            Logger.getLogger(ControlProveedorTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Collection<Proveedor> result = instance.todosLosProveedores();
+        assertNotNull(result);
+        for (Proveedor proveedor : result) {
+            System.out.println(proveedor.getNombre());
         }
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
