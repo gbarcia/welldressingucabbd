63512686ece5434e6ec7cd2f85d3e0a589efe83d
gerardobarciap <gerardobarciap@beabdc1e-be5c-11dd-b9c8-e7e19bf6fede>
2008-12-30 04:19:53 +0000

    actualizacion del caso de uso Proveedor

--- src/com/wd/gui/controlparticular/ControlGuiProveedor.java
+++ src/com/wd/gui/controlparticular/ControlGuiProveedor.java
@@ -123,4 +123,33 @@ public class ControlGuiProveedor {
         resultado = controlG.consultaProductosProveedor(rif);
         return resultado;
     }
+
+    /** operacion para editar un proveedor en el sistema
+     * @param rif rif del proveedor
+     * @param nombre nombre del proveedor
+     * @param telefono telefono del proveedor
+     * @param dir direccion del proveedor
+     * @param nombreC nombre del contacto
+     * @param apellidoC apellido del contacto
+     * @param lugar indice de la ciudad donde se encuentra ubicado
+     */
+    public void editarProveedorEnElSistema(String rif, String nombre, String telefono,
+            String dir, String nombreC, String apellidoC,
+            Integer lugar) {
+        boolean resultado = false;
+        boolean datosValidos = true;
+        datosValidos = this.validarDatosForm(rif, nombre, telefono, dir, nombreC, apellidoC);
+        if (datosValidos) {
+            proveedor = new Proveedor(rif, nombre, telefono, dir,
+                    lugar, nombreC, apellidoC);
+            resultado = controlG.editarProveedor(proveedor);
+            if (resultado) {
+                controlador.mostrarMensaje("Proveedor " + nombre + " actualizado con éxito", 0);
+            } else {
+                controlador.mostrarMensaje("Operacion fallida", 1);
+            }
+        } else {
+            controlador.mostrarMensaje("Error: Todos los campos son requeridos", 1);
+        }
+    }
 }
