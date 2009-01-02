ec18f5a1ca86a9e744fc09e1883037e269a89bc4
gerardobarciap <gerardobarciap@beabdc1e-be5c-11dd-b9c8-e7e19bf6fede>
2009-01-02 08:07:53 +0000

    actualizacion del caso de uso EmpleadoTienda

--- src/com/wd/dominio/Empleado.java
+++ src/com/wd/dominio/Empleado.java
@@ -1,17 +1,42 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package com.wd.dominio;
 
+import java.sql.Date;
+import java.util.Collection;
+
 /**
- *
+ * Entidad para el manejo de la información de empleado para el sistema
  * @author gerardo
  */
 public class Empleado {
 
+    private int cedula;
+
+    private String nombre;
+
+    private String apellido;
+
+    private Date fechaNacimiento;
+
+    private String telefono;
+
+    private int estadoCivil;
+
+    private String sexo;
+
+    private int nivelEstudios;
+
+    private String direccion;
+
+    private int tipo;
+
+    private int lugarId;
+
+    private String ciudadVive;
+
+    private Collection<HistorialEmpleado> historial;
+
     public Empleado() {
     }
-
+    
 }
