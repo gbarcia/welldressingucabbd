25609f6c9b544f3ad241dca9039956f8d0438e34
gerardobarciap <gerardobarciap@beabdc1e-be5c-11dd-b9c8-e7e19bf6fede>
2009-01-02 19:02:00 +0000

    actualizacion del caso de uso EmpleadoTienda

--- src/com/wd/dominio/Empleado.java
+++ src/com/wd/dominio/Empleado.java
@@ -5,22 +5,66 @@ import java.util.Collection;
 
 /**
  * Entidad para el manejo de la información de empleado para el sistema
+ * maneja además su historial de registro de las tiendas donde ha estado durante
+ * un determinado periodo y su respectivo cargo
  * @author gerardo
  */
 public class Empleado {
 
+    /** cedula del empleado*/
     private int cedula;
+
+    /** nombre del empleado*/
     private String nombre;
+
+    /** apellido del empleado*/
     private String apellido;
+
+    /** fecha de nacimiento del empleado*/
     private Date fechaNacimiento;
+
+    /** telefono del empleado*/
     private String telefono;
+
+    /** estado civil del empleado
+     *  0 para soltero
+     *  1 para casado
+     *  2 para divorciado
+     *  3 para viudo
+     */
     private int estadoCivil;
+
+     /** sexo del empleado
+     *  M para masculino
+     *  F para feminino
+     */
     private String sexo;
+
+     /** nivel de estudios del empleado
+     *  0 para ninguno
+     *  1 para educacion primaria
+     *  2 para educacion secundaria
+     *  3 para educacion superior
+     */
     private int nivelEstudios;
+
+    /** direccion del empleado */
     private String direccion;
+
+     /** tipo o cargo del empleado
+     *  0 para gerente
+     *  1 para empleado regular
+     *  2 para conductor
+     */
     private int tipo;
+
+    /** clave indentificadora del lugar donde vive */
     private int lugarId;
+
+    /** nombre de la ciudad donde vive */
     private String ciudadVive;
+    
+    /** coleccion con todos sus historiales */
     private Collection<HistorialEmpleado> historial;
 
     public Empleado() {
@@ -60,8 +104,9 @@ public class Empleado {
         this.ciudadVive = ciudadVive;
     }
 
-    private HistorialEmpleado nuevoObjetoHistorial () {
-        return new HistorialEmpleado();
+    private void nuevoObjetoHistorial () {
+        HistorialEmpleado he = new HistorialEmpleado();
+        this.historial.add(he);
     }
 
     public String getApellido() {
