ae61433de38951937ab3242bef84132e7ed0b07d
gerardobarciap <gerardobarciap@beabdc1e-be5c-11dd-b9c8-e7e19bf6fede>
2009-01-02 18:44:36 +0000

    actualizacion del caso de uso EmpleadoTienda

--- src/com/wd/dominio/HistorialEmpleado.java
+++ src/com/wd/dominio/HistorialEmpleado.java
@@ -12,7 +12,7 @@ public class HistorialEmpleado {
 
     private int cedula;
 
-    private int rif;
+    private String rif;
 
     private Date fechaInicio;
 
@@ -23,4 +23,52 @@ public class HistorialEmpleado {
     public HistorialEmpleado() {
     }
 
+    public HistorialEmpleado(int cedula, String rif, Date fechaInicio,
+            Date fechaFin, int cargo) {
+        this.cedula = cedula;
+        this.rif = rif;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.cargo = cargo;
+    }
+
+    public int getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(int cargo) {
+        this.cargo = cargo;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getRif() {
+        return rif;
+    }
+
+    public void setRif(String rif) {
+        this.rif = rif;
+    }
 }
