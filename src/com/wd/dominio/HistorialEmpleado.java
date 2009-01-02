f5b74f3e619a1d33cb4687b8cd3c263c7ad26643
gerardobarciap <gerardobarciap@beabdc1e-be5c-11dd-b9c8-e7e19bf6fede>
2009-01-02 08:11:07 +0000

    actualizacion del caso de uso EmpleadoTienda

--- src/com/wd/dominio/HistorialEmpleado.java
+++ src/com/wd/dominio/HistorialEmpleado.java
@@ -1,15 +1,24 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package com.wd.dominio;
 
+import java.sql.Date;
+
 /**
- *
+ * Clase para el manejo de la informacion de cada movimiento historial de un
+ * empleado en el sistema
  * @author gerardo
  */
 public class HistorialEmpleado {
+
+    private int cedula;
+
+    private int rif;
+
+    private Date fechaInicio;
+
+    private Date fechaFin;
+
+    private int cargo;
 
     public HistorialEmpleado() {
     }
