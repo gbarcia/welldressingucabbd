223256e25066741486663da5c3b2cf87b16c03b9
gerardobarciap <gerardobarciap@beabdc1e-be5c-11dd-b9c8-e7e19bf6fede>
2009-01-17 19:30:00 +0000

    Gestion Orden Compra

---
+++ src/com/wd/servicios/ControlPedido.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.wd.servicios;
+
+/**
+ *
+ * @author Casa
+ */
+public class ControlPedido implements IfaceSolicitud {
+
+    public boolean actualizarInventario(Object aAct) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ src/com/wd/servicios/RegistroSolicitud.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.wd.servicios;
+
+/**
+ *
+ * @author Casa
+ */
+public class RegistroSolicitud {
+
+}
