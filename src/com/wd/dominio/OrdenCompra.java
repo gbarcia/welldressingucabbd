3618ae0e5070b4e5e48e44a65d1e6b29e711ed42
gerardobarciap <gerardobarciap@beabdc1e-be5c-11dd-b9c8-e7e19bf6fede>
2009-01-17 18:55:51 +0000

    Gestion Orden Compra

--- src/com/wd/dominio/OrdenCompra.java
+++ src/com/wd/dominio/OrdenCompra.java
@@ -1,6 +1,8 @@
 
 package com.wd.dominio;
 
+import java.util.Collection;
+
 /**
  *
  * @author Gerardo Barcia
@@ -9,7 +11,34 @@ public class OrdenCompra extends Solicitud {
 
     private Integer CentroCodigo;
 
-    private Integer ProvedorCodigo;
+    private Integer ProveedorCodigo;
+
+    private Integer placaCamion;
+
+    private Integer idConductor;
+
+    private String nombreCentro;
+
+    private String nombreProveedor;
+
+    private String nombreConductor;
+
+    public OrdenCompra(Integer CentroCodigo, Integer ProveedorCodigo, Integer placaCamion, Integer idConductor) {
+        this.CentroCodigo = CentroCodigo;
+        this.ProveedorCodigo = ProveedorCodigo;
+        this.placaCamion = placaCamion;
+        this.idConductor = idConductor;
+    }
+
+    public OrdenCompra(Integer CentroCodigo, Integer ProveedorCodigo, Integer placaCamion, Integer idConductor, String nombreCentro, String nombreProveedor, String nombreConductor) {
+        this.CentroCodigo = CentroCodigo;
+        this.ProveedorCodigo = ProveedorCodigo;
+        this.placaCamion = placaCamion;
+        this.idConductor = idConductor;
+        this.nombreCentro = nombreCentro;
+        this.nombreProveedor = nombreProveedor;
+        this.nombreConductor = nombreConductor;
+    }
 
     public Integer getCentroCodigo() {
         return CentroCodigo;
@@ -19,11 +48,61 @@ public class OrdenCompra extends Solicitud {
         this.CentroCodigo = CentroCodigo;
     }
 
-    public Integer getProvedorCodigo() {
-        return ProvedorCodigo;
+    public Integer getProveedorCodigo() {
+        return ProveedorCodigo;
     }
 
-    public void setProvedorCodigo(Integer ProvedorCodigo) {
-        this.ProvedorCodigo = ProvedorCodigo;
-    }        
+    public void setProveedorCodigo(Integer ProveedorCodigo) {
+        this.ProveedorCodigo = ProveedorCodigo;
+    }
+
+    public Integer getIdConductor() {
+        return idConductor;
+    }
+
+    public void setIdConductor(Integer idConductor) {
+        this.idConductor = idConductor;
+    }
+
+    public String getNombreCentro() {
+        return nombreCentro;
+    }
+
+    public void setNombreCentro(String nombreCentro) {
+        this.nombreCentro = nombreCentro;
+    }
+
+    public String getNombreConductor() {
+        return nombreConductor;
+    }
+
+    public void setNombreConductor(String nombreConductor) {
+        this.nombreConductor = nombreConductor;
+    }
+
+    public String getNombreProveedor() {
+        return nombreProveedor;
+    }
+
+    public void setNombreProveedor(String nombreProveedor) {
+        this.nombreProveedor = nombreProveedor;
+    }
+
+    public Integer getPlacaCamion() {
+        return placaCamion;
+    }
+
+    public void setPlacaCamion(Integer placaCamion) {
+        this.placaCamion = placaCamion;
+    }
+
+    public Collection<Item> getColeccionProductos() {
+        return coleccionProductos;
+    }
+
+    public void setColeccionProductos(Collection<Item> coleccionProductos) {
+        this.coleccionProductos = coleccionProductos;
+    }
 }
+
+  
