4d79a26b8fccf31f9674011b4e978f208644a2c8
gerardobarciap <gerardobarciap@beabdc1e-be5c-11dd-b9c8-e7e19bf6fede>
2009-01-18 04:09:56 +0000

    Gestion Orden Compra

---
+++ src/com/wd/dominio/Inventario.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.wd.dominio;
+
+/**
+ *
+ * @author Casa
+ */
+public class Inventario {
+
+}
