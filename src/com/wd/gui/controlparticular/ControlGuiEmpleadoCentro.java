0bbd122647206efba9d1e47a46736dbb993c7826
trujillo.jonathan <trujillo.jonathan@beabdc1e-be5c-11dd-b9c8-e7e19bf6fede>
2009-01-18 00:22:40 +0000

    caso de uso: gestion empleados de centro

--- src/com/wd/gui/controlparticular/ControlGuiEmpleadoCentro.java
+++ src/com/wd/gui/controlparticular/ControlGuiEmpleadoCentro.java
@@ -2,8 +2,11 @@ package com.wd.gui.controlparticular;
 
 import com.wd.dominio.Empleado;
 import com.wd.dominio.NominaCentro;
+import com.wd.gui.ControlGui;
 import com.wd.servicios.ControlGeneral;
+import com.wd.servicios.IfaceControlGeneral;
 import java.util.Collection;
+import java.util.Date;
 import java.util.Vector;
 
 /**
@@ -12,7 +15,16 @@ import java.util.Vector;
  */
 public class ControlGuiEmpleadoCentro{
 
+    /* variable para el manejo de la entidad empleado  */
+    private Empleado empleado;
+    /** variable para el manejo de la instancia del controlador GUI */
+    private ControlGui controlador;
+    /** variable de la interface responsable del control de las entidades */
+    private IfaceControlGeneral controlG = ControlGeneral.getInstance();
+
+    /** constructor que inicia el control general GUI */
     public ControlGuiEmpleadoCentro() {
+        this.controlador = new ControlGui();
     }
 
     /** Operacion para conocer si un string es número
@@ -51,6 +63,55 @@ public class ControlGuiEmpleadoCentro{
         return resultado;
     }
 
+    /**Operacion para registrar un Empleado en el sistema
+     * operacion para registrar un nuevo empleado en el sistema
+     * @param cedula int numero de cedula
+     * @param nombre String nombre del empleado
+     * @param apellido String apellido del empleado
+     * @param fechaNacimiento java.sql.Date la fecha de nacimiento del empleado
+     * @param telefono String telefono del empleado
+     * @param estadoCivil int estado civil del empleado
+     * @param sexo String sexo del empleado
+     * @param nivelEstudios int nivel de estudios del empleado
+     * @param direccion String direccion del empleado
+     * @param tipo int tipo de cargo del empleado
+     * @param lugarId int el indice de la ciudad donde vive el empleado
+     * @param ciudadVive String nombre de la ciudad donde vive el empleado
+     * @param codigo int codigo del centro donde trabajara el empleado
+     * @param nombreEmpresa String nombre de la empresa o tienda donde trabajara
+     * el empleado
+     */
+    public void registrarNuevoEmpleado(String cedula, String nombre, String apellido,
+            java.sql.Date fechaNacimiento, String telefono, int estadoCivil, String sexo,
+            int nivelEstudios, String direccion, int tipo, int lugarId, String ciudadVive,
+            int codigo, String nombreEmpresa) {
+        boolean resultado = false;
+        boolean datosValidos = false;
+        boolean esNumero = false;
+        esNumero = this.isNumber(cedula);
+        if (esNumero) {
+            datosValidos = this.validarForm(sexo, nombre, apellido, telefono, direccion);
+            int ci = Integer.parseInt(cedula); // conviertiendo la cedula en numero entero
+            if (datosValidos) {
+                Date fecha = new Date();
+                java.sql.Date fechaSQL = new java.sql.Date(fecha.getTime()); //Obteniendo la fecha actual
+                empleado = new Empleado(ci, nombre, apellido, fechaNacimiento, telefono,
+                        estadoCivil, sexo, nivelEstudios, direccion, tipo, lugarId,
+                        ciudadVive, fechaSQL, null, codigo, nombreEmpresa);
+                resultado = controlG.agregarEmpleadoTienda(empleado);
+                if (resultado) {
+                    controlador.mostrarMensaje("Empleado " + nombre + " agregado con éxito", 0);
+                } else if (!resultado) {
+                    controlador.mostrarMensaje("Operacion fallida", 1);
+                }
+            } else if (!datosValidos) {
+                controlador.mostrarMensaje("Error: Todos los campos son requeridos", 1);
+            }
+        } else if (!esNumero) {
+            controlador.mostrarMensaje("Error: el campo cedula debe ser número", 1);
+        }
+    }
+
    /**
     * Metodo consultar el historial de empleados de los centros de distribucion
     * @return Vector vector con todos los objetos EmpresaVigilancia del sistema
@@ -76,8 +137,8 @@ public class ControlGuiEmpleadoCentro{
         Vector<Empleado> vectorResult = null;
         Collection<Empleado> coleccion = ControlGeneral.getInstance().traerTodosLosEmpleadosCentro(codigoCentro);
         vectorResult = new Vector();
-        for (Empleado empleado : coleccion) {
-            vectorResult.add(empleado);
+        for (Empleado e : coleccion) {
+            vectorResult.add(e);
         }
         return vectorResult;
     }
